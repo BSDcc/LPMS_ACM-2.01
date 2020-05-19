10ac0faaed416d7534666b0c3175778f1418349d
Francois Meyer <francois@bluecrane.cc>
2020-05-19 10:45:03 +0200

    [20200519] Merged Change

--- lpms_excel.pas
+++ lpms_excel.pas
@@ -303,6 +303,9 @@ begin
          xlsSheet.WriteBorders(idx1,Col,[cbWest,cbSouth,cbNorth]);
          xlsSheet.WriteBorders(idx1,Col+1,[cbSouth,cbNorth,cbEast]);
          xlsSheet.WriteBorders(idx1,Col+2,[cbSouth,cbNorth,cbEast]);
+         xlsSheet.WriteBackgroundColor(idx1,Col,clSilver);
+         xlsSheet.WriteBackgroundColor(idx1,Col+1,clSilver);
+         xlsSheet.WriteFontStyle(idx1,Col,[fssBold]);
 
       end;
 
@@ -452,383 +455,84 @@ begin
 
    end;
 
-//--- Save the spreadsheet to a file
-
-   xlsBook.WriteToFile(FileName + '.xlsx', True);
-   xlsBook.Free;
-
-{
-var
-   row, col  : integer;          // Row and Column counter
-   ThisCase  : integer;          // Used for case statements
-   ThisPref  : string;           // Current Company Prefix
-
-   xlsBook   : IXLSWorkbook;
-   xlsSheet  : IXLSWorksheet;
-
-begin
-   ShortDateFormat := 'yyyy/MM/dd';
-   DateSeparator   := '/';
-
-//--- Create the Excel workbook that will contain the exported data
-
-   xlsBook := TXLSWorkbook.Create;
-
-//--- Open the database connection and read the Companies table
-
-   if (OpenDB = false) then begin
-      MessageDlg('Unexpected error while opening database connection: ' + ErrMsg + '. LPMS_Export will now terminate', mtWarning, [mbOK], 0);
-      Close;
-      Exit;
-   end;
-
-   if (ReadCpy = false) then begin
-      MessageDlg('Unexpected database error:' + ErrMsg + '. LPMS_Export will now terminate', mtWarning, [mbOK], 0);
-      Close;
-      Exit;
-   end;
-
-   while (sqlQry1.Eof = false) do begin
-      ThisPref := sqlQry1.FieldByName('LPMSKey_Prefix').AsString;
-
-      row := 1;
-      col := 1;
-
-      xlsSheet := xlsBook.Sheets.Add;
-      xlsSheet.Name := sqlQry1.FieldByName('LPMSKey_Prefix').AsString;
-
-//--- Create the heading information
-
-      xlsSheet.Cells.Item[row,col].Value := 'LPMS Access Control Manager - Company and User definition export';
-      with xlsSheet.Range['A1', 'N1'] do begin
-         Borders[xlAround].Weight := xlThin;
-         Interior.Color           := clNavy;
-         Font.Color               := clWhite;
-         Font.Bold                := true;
-      end;
-
-      row := row + 2;
-
-      xlsSheet.Cells.Item[row,col].Value := 'Company:';
-      xlsSheet.Cells.Item[row,col].Borders[xlEdgeBottom].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col + 1].Borders[xlEdgeBottom].Weight := xlThin;
-
-      xlsSheet.Cells.Item[row + 1,col].Value := 'Prefix:';
-      xlsSheet.Cells.Item[row + 1,col].Borders[xlEdgeBottom].Weight := xlThin;
-      xlsSheet.Cells.Item[row + 1,col + 1].Borders[xlEdgeBottom].Weight := xlThin;
-
-      xlsSheet.Cells.Item[row + 2,col].Value := 'Type:';
-      xlsSheet.Cells.Item[row + 2,col].Borders[xlEdgeBottom].Weight := xlThin;
-      xlsSheet.Cells.Item[row + 2,col + 1].Borders[xlEdgeBottom].Weight := xlThin;
-
-      xlsSheet.Cells.Item[row + 3,col].Value := 'License Count:';
-      xlsSheet.Cells.Item[row + 3,col].Borders[xlEdgeBottom].Weight := xlThin;
-      xlsSheet.Cells.Item[row + 3,col + 1].Borders[xlEdgeBottom].Weight := xlThin;
-
-      xlsSheet.Cells.Item[row + 4,col].Value := 'Interval:';
-      xlsSheet.Cells.Item[row + 4,col].Borders[xlEdgeBottom].Weight := xlThin;
-      xlsSheet.Cells.Item[row + 4,col + 1].Borders[xlEdgeBottom].Weight := xlThin;
-
-      xlsSheet.Cells.Item[row + 5,col].Value := 'Blocked:';
-      xlsSheet.Cells.Item[row + 5,col].Borders[xlEdgeBottom].Weight := xlThin;
-      xlsSheet.Cells.Item[row + 5,col + 1].Borders[xlEdgeBottom].Weight := xlThin;
-
-      with xlsSheet.Range['A3', 'B8'] do begin
-         Borders[xlAround].Weight := xlThin;
-         Interior.Color           := clSilver;
-         Font.Bold                := true;
-      end;
-
-      row := row + 7;
-
-//--- Print the table headings
-
-      xlsSheet.Cells.Item[row,col].Value := 'Key';
-      xlsSheet.Cells.Item[row,col].ColumnWidth := 5;
-      xlsSheet.Cells.Item[row,col].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col +  1].Value := 'User';
-      xlsSheet.Cells.Item[row,col +  1].ColumnWidth := 30;
-      xlsSheet.Cells.Item[row,col +  1].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col +  1].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col +  1].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col +  1].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col +  2].Value := 'Company';
-      xlsSheet.Cells.Item[row,col +  2].ColumnWidth := 35;
-      xlsSheet.Cells.Item[row,col +  2].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col +  2].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col +  2].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col +  2].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col +  3].Value := 'Email';
-      xlsSheet.Cells.Item[row,col +  3].ColumnWidth := 30;
-      xlsSheet.Cells.Item[row,col +  3].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col +  3].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col +  3].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col +  3].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col +  4].Value := 'Contact';
-      xlsSheet.Cells.Item[row,col +  4].ColumnWidth := 15;
-      xlsSheet.Cells.Item[row,col +  4].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col +  4].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col +  4].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col +  4].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col +  5].Value := 'Unique';
-      xlsSheet.Cells.Item[row,col +  5].ColumnWidth := 15;
-      xlsSheet.Cells.Item[row,col +  5].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col +  5].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col +  5].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col +  5].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col +  6].Value := 'License';
-      xlsSheet.Cells.Item[row,col +  6].ColumnWidth := 20;
-      xlsSheet.Cells.Item[row,col +  6].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col +  6].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col +  6].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col +  6].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col +  7].Value := 'Expire';
-      xlsSheet.Cells.Item[row,col +  7].ColumnWidth := 12;
-      xlsSheet.Cells.Item[row,col +  7].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col +  7].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col +  7].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col +  7].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col +  8].Value := 'Activation Key';
-      xlsSheet.Cells.Item[row,col +  8].ColumnWidth := 45;
-      xlsSheet.Cells.Item[row,col +  8].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col +  8].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col +  8].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col +  8].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col +  9].Value := 'Blocked';
-      xlsSheet.Cells.Item[row,col +  9].ColumnWidth := 12;
-      xlsSheet.Cells.Item[row,col +  9].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col +  9].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col +  9].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col +  9].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col + 10].Value := 'Renewals';
-      xlsSheet.Cells.Item[row,col + 10].ColumnWidth := 12;
-      xlsSheet.Cells.Item[row,col + 10].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col + 10].HorizontalAlignment := xlHalignRight;
-      xlsSheet.Cells.Item[row,col + 10].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col + 10].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col + 10].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col + 11].Value := 'Transfer';
-      xlsSheet.Cells.Item[row,col + 11].ColumnWidth := 12;
-      xlsSheet.Cells.Item[row,col + 11].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col + 11].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col + 11].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col + 11].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col + 12].Value := 'New Prefix';
-      xlsSheet.Cells.Item[row,col + 12].ColumnWidth := 12;
-      xlsSheet.Cells.Item[row,col + 12].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col + 12].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col + 12].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col + 12].Font.Bold := true;
-
-      xlsSheet.Cells.Item[row,col + 13].Value := 'New License';
-      xlsSheet.Cells.Item[row,col + 13].ColumnWidth := 20;
-      xlsSheet.Cells.Item[row,col + 13].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row,col + 13].Interior.Color := clNavy;
-      xlsSheet.Cells.Item[row,col + 13].Font.Color := clWhite;
-      xlsSheet.Cells.Item[row,col + 13].Font.Bold := true;
-
-//--- Insert the Company Information
-
-      row := 3;
-      col := 3;
-
-      xlsSheet.Cells.Item[row,col].Value := sqlQry1.FieldByName('LPMSKey_Desc').AsString;
-      xlsSheet.Cells.Item[row,col].Borders[xlAround].Weight     := xlThin;
-      xlsSheet.Cells.Item[row,col].Interior.Color := clSilver;
-
-      xlsSheet.Cells.Item[row + 1,col].Value := sqlQry1.FieldByName('LPMSKey_Prefix').AsString;
-      xlsSheet.Cells.Item[row + 1,col].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row + 1,col].Interior.Color := clSilver;
-
-      xlsSheet.Cells.Item[row + 2,col].Value := sqlQry1.FieldByName('LPMSKey_Name').AsString;
-      xlsSheet.Cells.Item[row + 2,col].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row + 2,col].Interior.Color := clSilver;
-
-      xlsSheet.Cells.Item[row + 3,col].Value := sqlQry1.FieldByName('LPMSKey_LicCount').AsString;
-      xlsSheet.Cells.Item[row + 3,col].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row + 3,col].Interior.Color := clSilver;
-
-      xlsSheet.Cells.Item[row + 4,col].Value := sqlQry1.FieldByName('LPMSKey_Interval').AsString;
-      xlsSheet.Cells.Item[row + 4,col].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row + 4,col].Interior.Color := clSilver;
-
-      if (sqlQry1.FieldByName('LPMSKey_Blocked').AsInteger = 0) then
-         xlsSheet.Cells.Item[row + 5,col].Value := 'No'
-      else
-         xlsSheet.Cells.Item[row + 5,col].Value := 'Yes';
-
-      xlsSheet.Cells.Item[row + 5,col].Borders[xlAround].Weight := xlThin;
-      xlsSheet.Cells.Item[row + 5,col].Interior.Color := clSilver;
-
-      row := row + 8;
-      col := 1;
-
-//--- Insert the User Information for this Company
-
-      ReadUser(ThisPref);
-
-      while (sqlQry2.Eof = false) do begin
-         xlsSheet.Cells.Item[row,col].Value := sqlQry2.FieldByName('LPMSKey_Key').AsString;
-         xlsSheet.Cells.Item[row,col].Borders[xlAround].Weight := xlThin;
-
-         xlsSheet.Cells.Item[row,col +  1].Value := sqlQry2.FieldByName('LPMSKey_Name').AsString;
-         xlsSheet.Cells.Item[row,col +  1].Borders[xlAround].Weight := xlThin;
-
-         xlsSheet.Cells.Item[row,col +  2].Value := sqlQry2.FieldByName('LPMSKey_Company').AsString;
-         xlsSheet.Cells.Item[row,col +  2].Borders[xlAround].Weight := xlThin;
-
-         xlsSheet.Cells.Item[row,col +  3].Value := sqlQry2.FieldByName('LPMSKey_Email').AsString;
-         xlsSheet.Cells.Item[row,col +  3].Borders[xlAround].Weight := xlThin;
-
-         xlsSheet.Cells.Item[row,col +  4].Value := sqlQry2.FieldByName('LPMSKey_Contact').AsString;
-         xlsSheet.Cells.Item[row,col +  4].Borders[xlAround].Weight := xlThin;
-
-         xlsSheet.Cells.Item[row,col +  5].Value := sqlQry2.FieldByName('LPMSKey_Unique').AsString;
-         xlsSheet.Cells.Item[row,col +  5].Borders[xlAround].Weight := xlThin;
-
-         ThisCase := sqlQry2.FieldByName('LPMSKey_LicType').AsInteger;
-         case ThisCase of
-             1: xlsSheet.Cells.Item[row,col +  6].Value := 'Trial';
-             2: xlsSheet.Cells.Item[row,col +  6].Value := 'Personal';
-             3: xlsSheet.Cells.Item[row,col +  6].Value := 'Browse';
-             4: xlsSheet.Cells.Item[row,col +  6].Value := 'Generic';
-         end;
-         xlsSheet.Cells.Item[row,col +  6].Borders[xlAround].Weight := xlThin;
-
-         xlsSheet.Cells.Item[row,col +  7].Value := sqlQry2.FieldByName('LPMSKey_ExpiryDate').AsString;
-         xlsSheet.Cells.Item[row,col +  7].Borders[xlAround].Weight := xlThin;
-
-         xlsSheet.Cells.Item[row,col +  8].Value := sqlQry2.FieldByName('LPMSKey_Activation').AsString;
-         xlsSheet.Cells.Item[row,col +  8].Borders[xlAround].Weight := xlThin;
-
-         if (sqlQry2.FieldByName('LPMSKey_Blocked').AsInteger = 0) then
-            xlsSheet.Cells.Item[row,col +  9].Value := 'No'
-         else
-            xlsSheet.Cells.Item[row,col +  9].Value := 'Yes';
-         xlsSheet.Cells.Item[row,col +  9].Borders[xlAround].Weight := xlThin;
-
-         xlsSheet.Cells.Item[row,col + 10].Value := sqlQry2.FieldByName('LPMSKey_Renewals').AsString;
-         xlsSheet.Cells.Item[row,col + 10].Borders[xlAround].Weight := xlThin;
-         xlsSheet.Cells.Item[row,col + 10].HorizontalAlignment := xlHalignRight;
-
-         if (sqlQry2.FieldByName('LPMSKey_Transfer').AsInteger = 0) then
-            xlsSheet.Cells.Item[row,col + 11].Value := 'No'
-         else
-            xlsSheet.Cells.Item[row,col + 11].Value := 'Yes';
-         xlsSheet.Cells.Item[row,col + 11].Borders[xlAround].Weight := xlThin;
-
-         xlsSheet.Cells.Item[row,col + 12].Value := sqlQry2.FieldByName('LPMSKey_NewPrefix').AsString;
-         xlsSheet.Cells.Item[row,col + 12].Borders[xlAround].Weight := xlThin;
-
-         ThisCase := sqlQry2.FieldByName('LPMSKey_NewLicense').AsInteger;
-         case ThisCase of
-             1: xlsSheet.Cells.Item[row,col + 13].Value := 'Trial';
-             2: xlsSheet.Cells.Item[row,col + 13].Value := 'Personal';
-             3: xlsSheet.Cells.Item[row,col + 13].Value := 'Browse';
-             4: xlsSheet.Cells.Item[row,col + 13].Value := 'Generic';
-         end;
-         xlsSheet.Cells.Item[row,col + 13].Borders[xlAround].Weight := xlThin;
-
-         row := row + 1;
-         sqlQry2.Next;
-      end;
-
-      sqlQry1.Next;
-   end;
-
 //--- Insert a sheet with all the Unique Identifiers
 
    if (ReadUnique = false) then begin
-      MessageDlg('Unexpected database error:' + ErrMsg + '. LPMS_Export will now terminate', mtWarning, [mbOK], 0);
+
+      Application.MessageBox(PChar('Unexpected database error:''' + ErrMsg + '''. LPMS_Export will now terminate'),'LPMS Export Manager', (MB_OK + MB_ICONWARNING));
       Close;
       Exit;
+
    end;
 
-   row := 1;
-   col := 1;
+   Row := 0;
+   Col := 0;
 
-   xlsSheet := xlsBook.Sheets.Add;
-   xlsSheet.Name := 'Unique Identifiers';
+   xlsSheet := xlsBook.AddWorksheet('Unique Identifiers');
 
 //--- Create the heading information
 
-   xlsSheet.Cells.Item[row,col].Value := 'LPMS Access Control Manager - Company and User definition export';
-   with xlsSheet.Range['A1', 'C1'] do begin
-      Borders[xlAround].Weight := xlThin;
-      Interior.Color           := clNavy;
-      Font.Color               := clWhite;
-      Font.Bold                := true;
+   xlsSheet.WriteText(Row,Col,'LPMS Access Control Manager - Company and User definition export');
+   xlsSheet.WriteBorders(Row,Col,[cbWest]);
+   xlsSheet.WriteBorders(Row,Col+2,[cbEast]);
+
+   for idx1 := Col to Col+2 do begin
+
+      xlsSheet.WriteBorders(Row,idx1,[cbNorth, cbSouth]);
+      xlsSheet.WriteBackgroundColor(Row,idx1,clNavy);
+      xlsSheet.WriteFontColor(Row,idx1,clWhite);
+      xlsSheet.WriteFontStyle(Row,idx1,[fssBold]);
+
    end;
 
-   row := row + 2;
+   Row := Row + 2;
 
 //--- Print the table headings
 
-   xlsSheet.Cells.Item[row,col].Value := 'Unique';
-   xlsSheet.Cells.Item[row,col].ColumnWidth := 15;
-   xlsSheet.Cells.Item[row,col].Borders[xlAround].Weight := xlThin;
-   xlsSheet.Cells.Item[row,col].Interior.Color := clNavy;
-   xlsSheet.Cells.Item[row,col].Font.Color := clWhite;
-   xlsSheet.Cells.Item[row,col].Font.Bold := true;
+   xlsSheet.WriteText(Row,Col,'Unique');
+   xlsSheet.WriteColWidth(Col,20);
 
-   xlsSheet.Cells.Item[row,col +  1].Value := 'User';
-   xlsSheet.Cells.Item[row,col +  1].ColumnWidth := 30;
-   xlsSheet.Cells.Item[row,col +  1].Borders[xlAround].Weight := xlThin;
-   xlsSheet.Cells.Item[row,col +  1].Interior.Color := clNavy;
-   xlsSheet.Cells.Item[row,col +  1].Font.Color := clWhite;
-   xlsSheet.Cells.Item[row,col +  1].Font.Bold := true;
+   xlsSheet.WriteText(Row,Col+1,'User');
+   xlsSheet.WriteColWidth(Col+1,40);
 
-   xlsSheet.Cells.Item[row,col +  2].Value := 'Company';
-   xlsSheet.Cells.Item[row,col +  2].ColumnWidth := 100;
-   xlsSheet.Cells.Item[row,col +  2].Borders[xlAround].Weight := xlThin;
-   xlsSheet.Cells.Item[row,col +  2].Interior.Color := clNavy;
-   xlsSheet.Cells.Item[row,col +  2].Font.Color := clWhite;
-   xlsSheet.Cells.Item[row,col +  2].Font.Bold := true;
+   xlsSheet.WriteText(Row,Col+2,'Company');
+   xlsSheet.WriteColWidth(Col+2,100);
 
-   row := row + 1;
+   for idx1 := Col to Col + 2 do begin
+
+      xlsSheet.WriteBorders(Row,idx1,[cbEast,cbNorth,cbSouth,cbWest]);
+      xlsSheet.WriteBackgroundColor(Row,idx1,clNavy);
+      xlsSheet.WriteFontColor(Row,idx1,clWhite);
+      xlsSheet.WriteFontStyle(Row,idx1,[fssBold]);
+
+   end;
+
+   Row := Row + 1;
 
 //--- Insert the Usernique Identifier details
 
    while (sqlQry2.Eof = false) do begin
-      xlsSheet.Cells.Item[row,col].Value := sqlQry2.FieldByName('LPMSKey_Unique').AsString;
-      xlsSheet.Cells.Item[row,col].Borders[xlAround].Weight := xlThin;
+
+      xlsSheet.WriteText(Row,Col,sqlQry2.FieldByName('LPMSKey_Unique').AsString);
+
       if (sqlQry2.FieldByName('LPMSKey_Unique').AsString = '123456789ABC') then
-         xlsSheet.Cells.Item[row,col].Font.Bold := true;
+         xlsSheet.WriteFontStyle(Row,Col,[fssBold]);
 
-      xlsSheet.Cells.Item[row,col +  1].Value := sqlQry2.FieldByName('LPMSKey_Name').AsString;
-      xlsSheet.Cells.Item[row,col +  1].Borders[xlAround].Weight := xlThin;
+      xlsSheet.WriteText(Row,Col+1,sqlQry2.FieldByName('LPMSKey_Name').AsString);
+      xlsSheet.WriteText(Row,Col+2,sqlQry2.FieldByName('LPMSKey_Company').AsString);
 
-      xlsSheet.Cells.Item[row,col +  2].Value := sqlQry2.FieldByName('LPMSKey_Company').AsString;
-      xlsSheet.Cells.Item[row,col +  2].Borders[xlAround].Weight := xlThin;
+      for idx1 := Col to Col + 2 do
+         xlsSheet.WriteBorders(Row,idx1,[cbWest,cbNorth,cbSouth,cbEast]);
 
-      row := row + 1;
+      Row := Row + 1;
       sqlQry2.Next;
+
    end;
 
-//--- Write the Excel file to disk
+//--- Save the spreadsheet to a file
 
-   xlsBook.SaveAs(edtOutFile.Text);
-
-//--- Now open the Excel workbook
-
-   ShellExecute(Handle,'open',PChar(edtOutFile.Text),nil,nil,SW_SHOWNORMAL);
-
-}
+   xlsBook.WriteToFile(FileName + '.xlsx', True);
+   xlsBook.Free;
 
    wsgGrid.LoadFromSpreadsheetFile(FileName + '.xlsx');
    wsgGrid.GetSheets(cbxSheets.Items);
@@ -1048,7 +752,7 @@ var
 
 begin
 
-   S1 := 'SELECT LPMSKey_Unique, LPMSKey_Name, LPMSKey_Company FROM users ORDER BY LPMSKey_Unique';
+   S1 := 'SELECT LPMSKey_Unique, LPMSKey_Name, LPMSKey_Company FROM users ORDER BY LPMSKey_Company ASC, LPMSKey_Name ASC';
 
    try
 
